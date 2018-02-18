b9bf0bc947f0548f8ced2bea314725088a107ccf
Jokubas Liutkus <liutkusjokubas@gmail.com>
2018-02-18 21:25:56 +0000

    refactored code

--- src/blockScan.cu
+++ src/blockScan.cu
@@ -15,7 +15,7 @@ if (err != cudaSuccess) {\
 }\
 }
 
-#define BLOCK_SIZE 256
+#define BLOCK_SIZE 512
 
 // for avoiding bank conflicts
 #define NUM_BANKS 32
@@ -76,6 +76,7 @@ void block_scan_full_BCAO(int *g_idata, int *g_odata, int n, int *SUM,
 	__shared__ int temp[BLOCK_SIZE << 2];  // allocated on invocation
 
 	int thid = threadIdx.x;
+	int thid_shift = thid << 1;
 	int blockId = blockDim.x * blockIdx.x << 1;
 	int offset = 0;  //1;
 	int last = 0;
@@ -98,8 +99,8 @@ void block_scan_full_BCAO(int *g_idata, int *g_odata, int n, int *SUM,
 			{
 		__syncthreads();
 		if (thid < d) {
-			int ai = (((thid << 1) + 1) << offset) - 1;
-			int bi = (((thid << 1) + 2) << offset) - 1;
+			int ai = ((thid_shift + 1) << offset) - 1;
+			int bi = ((thid_shift + 2) << offset) - 1;
 			ai += CONFLICT_FREE_OFFSET(ai);
 			bi += CONFLICT_FREE_OFFSET(bi);
 			temp[bi] += temp[ai];
@@ -117,8 +118,8 @@ void block_scan_full_BCAO(int *g_idata, int *g_odata, int n, int *SUM,
 		offset--;
 		__syncthreads();
 		if (thid < d) {
-			int ai = (((thid << 1) + 1) << offset) - 1;
-			int bi = (((thid << 1) + 2) << offset) - 1;
+			int ai = ((thid_shift + 1) << offset) - 1;
+			int bi = ((thid_shift + 2) << offset) - 1;
 			ai += CONFLICT_FREE_OFFSET(ai);
 			bi += CONFLICT_FREE_OFFSET(bi);
 			int t = temp[ai];
@@ -140,22 +141,23 @@ void block_scan_full_BCAO(int *g_idata, int *g_odata, int n, int *SUM,
 
 __host__
 void full_block_scan_BCAO(int *h_IN, int *h_OUT, int len) {
-// error code to check return calues for CUDA calss
+	// -----------------------------------------------------------
+	// 							INITIALIZATION
+	// -----------------------------------------------------------
+
+	// error code to check return values for CUDA class
 	cudaError_t err = cudaSuccess;
 
-// create host stopwatch times
-	StopWatchInterface * timer = NULL;
-	sdkCreateTimer(&timer);
-	double h_msecs;
-
+	// size to allocate for the vectors
 	size_t size = len * sizeof(int);
 
-// create device timer
+	// create device timer
 	cudaEvent_t d_start, d_stop;
 	float d_msecs;
 	cudaEventCreate(&d_start);
 	cudaEventCreate(&d_stop);
 
+	// allocate memory for all the possible vectors needed for the execution
 	int *d_IN = NULL;
 	err = cudaMalloc((void **) &d_IN, size);
 	CUDA_ERROR(err, "Failed to allocate device vector IN");
@@ -165,19 +167,12 @@ void full_block_scan_BCAO(int *h_IN, int *h_OUT, int len) {
 	CUDA_ERROR(err, "Failed to allocate device vector OUT");
 
 	int *d_SUM_1 = NULL;
-	err =
-			cudaMalloc((void**) &d_SUM_1,
-					1
-							+ ((len - 1) / (BLOCK_SIZE * 2))/*(int) ceil(len / (BLOCK_SIZE << 1))*/);
+	err = cudaMalloc((void**) &d_SUM_1, 1 + ((len - 1) / (BLOCK_SIZE * 2)));
 	CUDA_ERROR(err, "Failed to allocate device vector OUT");
 
-	printf("\n%d\n\n", (int) ceil(len / (BLOCK_SIZE << 1)));
-
 	int *d_SUM_1_Scanned = NULL;
-	err =
-			cudaMalloc((void**) &d_SUM_1_Scanned,
-					1
-							+ ((len - 1) / (BLOCK_SIZE * 2))/*(int) ceil(len / (BLOCK_SIZE << 1))*/);
+	err = cudaMalloc((void**) &d_SUM_1_Scanned,
+			1 + ((len - 1) / (BLOCK_SIZE * 2)));
 	CUDA_ERROR(err, "Failed to allocate device vector OUT");
 
 	int *d_SUM_2 = NULL;
@@ -188,59 +183,135 @@ void full_block_scan_BCAO(int *h_IN, int *h_OUT, int len) {
 	err = cudaMalloc((void**) &d_SUM_2_Scanned, BLOCK_SIZE << 1);
 	CUDA_ERROR(err, "Failed to allocate device vector OUT");
 
-// copy the memory from host to device
+	// copy the memory from the host to the device
 	err = cudaMemcpy(d_IN, h_IN, size, cudaMemcpyHostToDevice);
 	CUDA_ERROR(err, "Failed to copy array IN from host to device");
 
+	// size of the grid for each level
 	int blocksPerGridLevel1 = 1 + ((len - 1) / (BLOCK_SIZE * 2));
 	int blocksPerGridLevel2 = 1 + ceil(blocksPerGridLevel1 / (BLOCK_SIZE << 1));
+	int blocksPerGridLevel3 = 1 + ceil(blocksPerGridLevel2 / (BLOCK_SIZE << 1));
 
-	printf("\n%d\n\n", blocksPerGridLevel1);
-	printf("\n%d\n\n", blocksPerGridLevel2);
+	// -----------------------------------------------------------
+	// 							EXECUTION
+	// -----------------------------------------------------------
 
-	cudaEventRecord(d_start, 0);
-	block_scan_full_BCAO<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_IN, d_OUT, len,
-			d_SUM_1, 1);
-	block_scan_full_BCAO<<<blocksPerGridLevel2, BLOCK_SIZE>>>(d_SUM_1,
-			d_SUM_1_Scanned, blocksPerGridLevel1, d_SUM_2, 1);
-	block_scan_full_BCAO<<<1, BLOCK_SIZE>>>(d_SUM_2, d_SUM_2_Scanned,
-			blocksPerGridLevel2, NULL, 0);
-	add_to_block<<<blocksPerGridLevel2, BLOCK_SIZE>>>(d_SUM_1_Scanned,
-			d_SUM_2_Scanned);
-	add_to_block<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_OUT, d_SUM_1_Scanned);
-	cudaEventRecord(d_stop, 0);
-	cudaEventSynchronize(d_stop);
+	// choosing the level on which to run the kernels
+	// based on the size of the grids
 
-	cudaDeviceSynchronize();
+	// if level one grid size is equal to 1 then single
+	// LEVEL 1 is enough to scan the whole array
+	if (blocksPerGridLevel1 == 1) {
+		// record the start time
+		cudaEventRecord(d_start, 0);
+
+		// execute the actual kernel
+		block_scan_full_BCAO<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_IN, d_OUT,
+				len,
+				NULL, 0);
+
+		// record the stop time
+		cudaEventRecord(d_stop, 0);
+		cudaEventSynchronize(d_stop);
+		cudaDeviceSynchronize();
+	}
+
+	// if level two grid size is equal to 1 then two (LEVEL 2)
+	// scans are required to scan the whole array
+	else if (blocksPerGridLevel2 == 1) {
+		// record the start time
+		cudaEventRecord(d_start, 0);
+
+		// execute the actual kernels
+		block_scan_full_BCAO<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_IN, d_OUT,
+				len, d_SUM_1, 1);
+		block_scan_full_BCAO<<<blocksPerGridLevel2, BLOCK_SIZE>>>(d_SUM_1,
+				d_SUM_1_Scanned, blocksPerGridLevel1, NULL, 0);
+		add_to_block<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_OUT,
+				d_SUM_1_Scanned);
+
+		// record the stop time
+		cudaEventRecord(d_stop, 0);
+		cudaEventSynchronize(d_stop);
+		cudaDeviceSynchronize();
+	}
+
+	// if level 3 grid size is equal to 1 then three (LEVEL 3)
+	// scans are required to scan the whole array
+	else if (blocksPerGridLevel3 == 1) {
+		// record the start time
+		cudaEventRecord(d_start, 0);
+
+		// execute the actual kernels
+		block_scan_full_BCAO<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_IN, d_OUT,
+				len, d_SUM_1, 1);
+		block_scan_full_BCAO<<<blocksPerGridLevel2, BLOCK_SIZE>>>(d_SUM_1,
+				d_SUM_1_Scanned, blocksPerGridLevel1, d_SUM_2, 1);
+		block_scan_full_BCAO<<<1, BLOCK_SIZE>>>(d_SUM_2, d_SUM_2_Scanned,
+				blocksPerGridLevel2, NULL, 0);
+		add_to_block<<<blocksPerGridLevel2, BLOCK_SIZE>>>(d_SUM_1_Scanned,
+				d_SUM_2_Scanned);
+		add_to_block<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_OUT,
+				d_SUM_1_Scanned);
+
+		// record the stop time
+		cudaEventRecord(d_stop, 0);
+		cudaEventSynchronize(d_stop);
+		cudaDeviceSynchronize();
+	}
+
+	// if none of the conditions above is met, it means that the array is too
+	// large to be scanned in 3 level scan, therefore we print the error message
+	// and return
+	else {
+		fprintf(stderr,
+				"The array size=%d is too large to be scanned with level 3 scan!\n",
+				len);
+
+		// using goto is discouraged, however, in such situations
+		// where in the error conditions exit or cleanup is required
+		// it is considered idiomatic
+		goto cleanup;
+	}
+
+	// check whether the run was successful
 	err = cudaGetLastError();
 	CUDA_ERROR(err, "Failed to launch block scan kernel");
 
+	// get the duration it took for the kernels to execute
 	err = cudaEventElapsedTime(&d_msecs, d_start, d_stop);
 	CUDA_ERROR(err, "Failed to get elapsed time");
 
-	printf("Block scan with single thread of %d elements took = %.f5mSecs\n",
+	// print the time elapsed
+	printf(
+			"Full block with bank avoidance scan with %d elements took = %.f5mSecs\n",
 			len, d_msecs);
 
+	// copy the result from the device back to the host
 	err = cudaMemcpy(h_OUT, d_OUT, size, cudaMemcpyDeviceToHost);
 	CUDA_ERROR(err, "Failed to copy array OUT from device to host");
 
-//// cleanup
-// Free device global memory
-	err = cudaFree(d_IN);
-	CUDA_ERROR(err, "Failed to free device vector A");
-	err = cudaFree(d_OUT);
-	CUDA_ERROR(err, "Failed to free device vector B");
+	// -----------------------------------------------------------
+	// 							CLEANUP
+	// -----------------------------------------------------------
 
-// Clean up the Host timer
-	sdkDeleteTimer(&timer);
+	cleanup:
+	// Free device global memory
+	CUDA_ERROR(cudaFree(d_IN), "Failed to free device vector IN");
+	CUDA_ERROR(cudaFree(d_OUT), "Failed to free device vector OUT");
+	CUDA_ERROR(cudaFree(d_SUM_1), "Failed to free device vector B");
+	CUDA_ERROR(cudaFree(d_SUM_1_Scanned), "Failed to free device vector B");
+	CUDA_ERROR(cudaFree(d_SUM_2), "Failed to free device vector B");
+	CUDA_ERROR(cudaFree(d_SUM_2_Scanned), "Failed to free device vector B");
 
-// Clean up the Device timer event objects
+	// Clean up the Device timer event objects
 	cudaEventDestroy(d_start);
 	cudaEventDestroy(d_stop);
 
-// Reset the device and exit
+	// Reset the device and exit
 	err = cudaDeviceReset();
 	CUDA_ERROR(err, "Failed to reset the device");
+
 }
 
 __global__
@@ -490,7 +561,7 @@ int main(void) {
 	cudaEventCreate(&d_stop);
 
 // size of the array to add
-	int numElements = 10000;
+	int numElements = 10000000;
 	size_t size = numElements * sizeof(int);
 
 // allocate the memory on the host for the arrays
@@ -529,43 +600,47 @@ int main(void) {
 	CUDA_ERROR(err, "Failed to allocate device vector OUT");
 
 // copy the memory from host to device
-	err = cudaMemcpy(d_IN, h_IN, size, cudaMemcpyHostToDevice);
-	CUDA_ERROR(err, "Failed to copy array IN from host to device");
-	int blocksPerGridLevel1 = 1 + ((numElements - 1) / (BLOCK_SIZE * 2));
-
-	cudaEventRecord(d_start, 0);
-	block_scan_full_BCAO<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_IN, d_OUT,
-			numElements,
-			NULL, 0);
-
-	cudaEventRecord(d_stop, 0);
-	cudaEventSynchronize(d_stop);
-
-	cudaDeviceSynchronize();
-	err = cudaGetLastError();
-	CUDA_ERROR(err, "Failed to launch block scan kernel");
-
-	err = cudaEventElapsedTime(&d_msecs, d_start, d_stop);
-	CUDA_ERROR(err, "Failed to get elapsed time");
-
-	printf("Block scan with single thread of %d elements took = %.f5mSecs\n",
-			numElements, d_msecs);
-
-	err = cudaMemcpy(h_OUT_CUDA, d_OUT, size, cudaMemcpyDeviceToHost);
-	CUDA_ERROR(err, "Failed to copy array OUT from device to host");
-
-	//// cleanup
-	// Free device global memory
-	err = cudaFree(d_IN);
-	CUDA_ERROR(err, "Failed to free device vector A");
-	err = cudaFree(d_OUT);
-	CUDA_ERROR(err, "Failed to free device vector B");
-
-	compare_results(h_OUT, h_OUT_CUDA, numElements);
-////
-//	full_block_scan_BCAO(h_IN, h_OUT_CUDA, numElements);
+//	err = cudaMemcpy(d_IN, h_IN, size, cudaMemcpyHostToDevice);
+//	CUDA_ERROR(err, "Failed to copy array IN from host to device");
+//	int blocksPerGridLevel1 = 1 + ((numElements - 1) / (BLOCK_SIZE * 2));
+//
+//	cudaEventRecord(d_start, 0);
+//	block_scan_full_BCAO<<<blocksPerGridLevel1, BLOCK_SIZE>>>(d_IN, d_OUT,
+//			numElements,
+//			NULL, 0);
+//
+//	cudaEventRecord(d_stop, 0);
+//	cudaEventSynchronize(d_stop);
+//
+//	cudaDeviceSynchronize();
+//	err = cudaGetLastError();
+//	CUDA_ERROR(err, "Failed to launch block scan kernel");
+//
+//	err = cudaEventElapsedTime(&d_msecs, d_start, d_stop);
+//	CUDA_ERROR(err, "Failed to get elapsed time");
+//
+//	printf("Block scan with single thread of %d elements took = %.f5mSecs\n",
+//			numElements, d_msecs);
+//
+//	err = cudaMemcpy(h_OUT_CUDA, d_OUT, size, cudaMemcpyDeviceToHost);
+//	CUDA_ERROR(err, "Failed to copy array OUT from device to host");
+//
+//	//// cleanup
+//	// Free device global memory
+//	err = cudaFree(d_IN);
+//	CUDA_ERROR(err, "Failed to free device vector A");
+//	err = cudaFree(d_OUT);
+//	CUDA_ERROR(err, "Failed to free device vector B");
 //
 //	compare_results(h_OUT, h_OUT_CUDA, numElements);
+////
+	full_block_scan_BCAO(h_IN, h_OUT_CUDA, numElements);
+
+	compare_results(h_OUT, h_OUT_CUDA, numElements);
+
+	full_block_scan(h_IN, h_OUT_CUDA, numElements);
+
+	compare_results(h_OUT, h_OUT_CUDA, numElements);
 
 // Free host memory
 	free(h_IN);
